0c63fca3a74ecfc741411bc596c02add5e88a802
John Arley Cano <john.cano@devimed.com.co>
2024-03-11 22:51:06 -0500

    Base de datos conectada

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'telemetria',
+        'USER': 'admin',
+        'PASSWORD': '112112jA*',
+        'HOST': 'eia.c6a1qqdnkvko.us-east-1.rds.amazonaws.com',
     }
 }
 
