39040f259f6b9116887d2199f24ece6eac677e22
John Arley Cano <john.cano@devimed.com.co>
2024-03-11 23:10:35 -0500

    Modelos creados

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ubicaciones(models.Model):
+    nombre = models.CharField(max_length=255)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add = True)
+
+class Dispositivos(models.Model):
+    unidad = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
+    ubicacion = models.ForeignKey(Ubicaciones, on_delete=models.CASCADE)
+
+class Puntos(models.Model):
+    valor = models.FloatField()
+    fecha_creacion = models.DateField(auto_now_add = True)
+    dispositivo = models.ForeignKey(Dispositivos, on_delete=models.CASCADE)
